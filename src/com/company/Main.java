cd152c506b1aa90730f8114a098ca707c8ada7fb
timofeya123 <timofeya123@inbox.ru>
2021-07-12 14:37:31 +0600

    RSS_comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,30 +7,31 @@ import java.net.URL;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("URL = https://lenta.ru/rss/news");
-        System.out.println(readRSS("https://lenta.ru/rss/news"));
+        System.out.println("URL = https://lenta.ru/rss/news"); // пометка, что мы будем брать
+        System.out.println(readRSS("https://lenta.ru/rss/news")); // вывести на экран значения из метода с URL ссылкой
     }
 
-    public static String readRSS(String URLAdress){
+    public static String readRSS(String URLAdress){ // наш метод с URL ссылкой
         try {
-            URL rssURL = new URL(URLAdress);
-            BufferedReader in = new BufferedReader(new InputStreamReader(rssURL.openStream()));
-            String sourceCode = "";
-            String line;
-            while ((line=in.readLine())!=null){
-                if (line.contains("<title>")){
-                    int firstPosition = line.indexOf("<title>");
-                    String temp = line.substring(firstPosition);
-                    temp = temp.replace("<title>", "");
-                    int lastPosition = temp.indexOf("</title>");
-                    temp = temp.substring(0, lastPosition);
-                    sourceCode += temp+"\n";
+            URL rssURL = new URL(URLAdress); // запихиваем наш URL
+            BufferedReader in = new BufferedReader(new InputStreamReader(rssURL.openStream())); // считываем поток данных в буфер
+            String sourceCode = ""; // пустая строчка с будущим текстом
+            String line; // переменная строка
+            //в котору при помощи цикла, будем считывать по строчки из буфера
+            while ((line=in.readLine())!=null){ // пока не пустой
+                if (line.contains("<title>")){ // если находит в строчке наше слово
+                    int firstPosition = line.indexOf("<title>"); // запоминаем его место
+                    String temp = line.substring(firstPosition); // во временую строчку закиним новую, но обрезаную
+                    temp = temp.replace("<title>", ""); // избавимься от нашего слова в строчке
+                    int lastPosition = temp.indexOf("</title>"); // находим место окончания
+                    temp = temp.substring(0, lastPosition); // обрезаем
+                    sourceCode += temp+"\n"; // закидываем то что получилось в сточку которую вернём
                 }
             }
-            in.close();
-            return sourceCode;
+            in.close(); // закрыть буфер
+            return sourceCode; // вернуть значения
         }catch (Exception e){
-            System.out.println(e);
+            System.out.println(e); // ошибка
         }
         return null;
     }
